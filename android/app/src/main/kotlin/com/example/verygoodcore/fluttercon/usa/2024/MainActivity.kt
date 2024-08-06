4225defd184be135e412672046e0c1ced9db8c26
Stefan <stefan@verygood.ventures>
2024-08-06 09:05:56 -0400

    adding project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/fluttercon/usa/2024/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.fluttercon_usa_2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:fluttercon_usa_2024/app/app.dart';
+import 'package:fluttercon_usa_2024/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
