57a5b0483abe70c5dfef43cc745fead8d81a8edb
Stefan Hodges-Kluck <125310677+verygoodstefan@users.noreply.github.com>
2024-08-07 08:52:40 -0400

    fix: github workflows (#3)

--- .github/update_github_actions.sh
+++ .github/update_github_actions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mason make github_actions_dart --on-conflict overwrite --exclude '' --minCoverage 100 --flutterVersion '3.22.1' --flutterChannel stable --dartChannel stable --dependabotFrequency daily --generateDependabot true --generateSemanticPullRequest true --generateSpellCheck true --spellCheckConfig cspell.json --workflowRef main --generateLicenseCheck true
+mason make github_actions_dart --on-conflict overwrite --exclude '' --minCoverage 100 --flutterVersion '3.22.1' --flutterChannel stable --dartChannel stable --dependabotFrequency daily --generateDependabot false --generateSemanticPullRequest true --generateSpellCheck true --spellCheckConfig cspell.json --workflowRef main --generateLicenseCheck false
