b31a990001c61b3c87a62dce743c82a73166b73a
Stefan <stefan@verygood.ventures>
2024-08-08 08:12:04 -0400

    data source package

---
+++ api/packages/fluttercon_data_source/lib/src/fluttercon_data_source.dart
@@ -0,0 +1,7 @@
+/// {@template fluttercon_data_source}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class FlutterconDataSource {
+  /// {@macro fluttercon_data_source}
+  const FlutterconDataSource();
+}
---
+++ api/packages/fluttercon_data_source/test/src/fluttercon_data_source_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:fluttercon_data_source/fluttercon_data_source.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FlutterconDataSource', () {
+    test('can be instantiated', () {
+      expect(FlutterconDataSource(), isNotNull);
+    });
+  });
+}
