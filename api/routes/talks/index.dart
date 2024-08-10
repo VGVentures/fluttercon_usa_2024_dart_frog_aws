b601b60200d0d8749f98e0176c2dc631719b37d4
Stefan <stefan@verygood.ventures>
2024-08-10 09:20:32 -0400

    filling in routes

--- api/routes/talks/index.dart
+++ api/routes/talks/index.dart
@@ -1,6 +1,18 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:convert';
+import 'dart:io';
 
-Response onRequest(RequestContext context) {
-  // TODO: implement route handler
-  return Response(body: 'This is a new route!');
+import 'package:dart_frog/dart_frog.dart';
+import 'package:fluttercon_data_source/fluttercon_data_source.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.get => await _get(context),
+    _ => Response(statusCode: HttpStatus.methodNotAllowed),
+  };
+}
+
+Future<Response> _get(RequestContext context) async {
+  final dataSource = context.read<FlutterconDataSource>();
+  final data = await dataSource.getTalks();
+  return Response(body: jsonEncode(data.toJson()));
 }
