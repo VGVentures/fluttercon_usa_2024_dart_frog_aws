5f61c56f51faa700c5482db7ecbdb02957cc1f0e
Stefan <stefan@verygood.ventures>
2024-08-13 11:16:54 -0400

    wip route

--- api/routes/index.dart
+++ api/routes/index.dart
@@ -1,5 +1,33 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:convert';
+import 'dart:io';
 
-Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+import 'package:dart_frog/dart_frog.dart';
+import 'package:fluttercon_data_source/fluttercon_data_source.dart';
+import 'package:user_repository/user_repository.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.get => await _get(context),
+    _ => Response(statusCode: HttpStatus.methodNotAllowed),
+  };
+}
+
+Future<Response> _get(RequestContext context) async {
+  final userRepository = context.read<UserRepository>();
+  try {
+    final user = await userRepository.getCurrentUser();
+    if (user == null) {
+      return Response(statusCode: HttpStatus.unauthorized);
+    }
+    final json = {
+      'id': user.id,
+      'sessionToken': user.sessionToken,
+    };
+    return Response(body: jsonEncode(json));
+  } on AmplifyApiException catch (e) {
+    return Response(
+      statusCode: HttpStatus.internalServerError,
+      body: jsonEncode(e.exception),
+    );
+  }
 }
