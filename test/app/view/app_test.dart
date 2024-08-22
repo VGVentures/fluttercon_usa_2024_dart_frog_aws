c5a8d8bac809e29d1e7ff2d2c17a1e68e0c5830f
Stefan <stefan@verygood.ventures>
2024-08-21 20:04:29 -0400

    adding dependency in test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fluttercon_api/fluttercon_api.dart';
 import 'package:fluttercon_usa_2024/app/app.dart';
 import 'package:fluttercon_usa_2024/counter/counter.dart';
+import 'package:mocktail/mocktail.dart';
+
+class _MockFlutterconApi extends Mock implements FlutterconApi {}
 
 void main() {
   group('App', () {
+    late FlutterconApi api;
+
+    setUp(() {
+      api = _MockFlutterconApi();
+    });
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
+      await tester.pumpWidget(App(
+        api: api,
+      ));
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
