33eebe7a1958c6ab24790de0c018ee6b385466b6
Stefan <stefan@verygood.ventures>
2024-09-02 14:23:16 -0400

    speakers bloc

---
+++ lib/speakers/bloc/speakers_state.dart
@@ -0,0 +1,40 @@
+part of 'speakers_bloc.dart';
+
+sealed class SpeakersState extends Equatable {
+  const SpeakersState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class SpeakersInitial extends SpeakersState {
+  const SpeakersInitial();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class SpeakersLoading extends SpeakersState {
+  const SpeakersLoading();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class SpeakersLoaded extends SpeakersState {
+  const SpeakersLoaded({required this.speakers});
+
+  final List<SpeakerPreview> speakers;
+
+  @override
+  List<Object> get props => [speakers];
+}
+
+final class SpeakersError extends SpeakersState {
+  const SpeakersError({required this.error});
+
+  final Object error;
+
+  @override
+  List<Object> get props => [error];
+}
---
+++ lib/speakers/speakers.dart
@@ -0,0 +1,2 @@
+export 'bloc/speakers_bloc.dart';
+export 'view/speakers_page.dart';
