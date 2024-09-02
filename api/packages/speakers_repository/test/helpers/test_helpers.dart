1bde2f0fa174833ca481118bbc5bbfa69f272fe4
Stefan <stefan@verygood.ventures>
2024-09-02 13:54:25 -0400

    speaker repository

---
+++ api/packages/speakers_repository/test/helpers/test_helpers.dart
@@ -0,0 +1,55 @@
+import 'package:fluttercon_data_source/fluttercon_data_source.dart';
+import 'package:fluttercon_shared_models/fluttercon_shared_models.dart';
+
+class TestHelpers {
+  static final speakers = PaginatedResult(
+    [
+      Speaker(
+        id: '1',
+        name: 'John Doe',
+        title: 'Test Title 1',
+        imageUrl: 'Test Image Url 1',
+      ),
+      Speaker(
+        id: '2',
+        name: 'Jane Doe',
+        title: 'Test Title 2',
+        imageUrl: 'Test Image Url 2',
+      ),
+      Speaker(
+        id: '3',
+        name: 'John Smith',
+        title: 'Test Title 3',
+        imageUrl: 'Test Image Url 3',
+      ),
+    ],
+    null,
+    null,
+    null,
+    Speaker.classType,
+    null,
+  );
+
+  static const speakerPreviews = PaginatedData(
+    items: [
+      SpeakerPreview(
+        id: '2',
+        name: 'Jane Doe',
+        title: 'Test Title 2',
+        imageUrl: 'Test Image Url 2',
+      ),
+      SpeakerPreview(
+        id: '1',
+        name: 'John Doe',
+        title: 'Test Title 1',
+        imageUrl: 'Test Image Url 1',
+      ),
+      SpeakerPreview(
+        id: '3',
+        name: 'John Smith',
+        title: 'Test Title 3',
+        imageUrl: 'Test Image Url 3',
+      ),
+    ],
+  );
+}
