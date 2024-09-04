3eaeaf493058d8d317c89b178b7ea2b2f34e6e13
Stefan <stefan@verygood.ventures>
2024-09-04 16:25:43 -0400

    reverting sdk update, adding vars for datetime test data

--- api/packages/talks_repository/test/helpers/test_helpers.dart
+++ api/packages/talks_repository/test/helpers/test_helpers.dart
@@ -2,9 +2,13 @@ import 'package:fluttercon_data_source/fluttercon_data_source.dart';
 import 'package:fluttercon_shared_models/fluttercon_shared_models.dart';
 
 class TestHelpers {
-  static final talk1StartTime = TemporalDateTime(DateTime(2024, 1, 3));
-  static final talk2StartTime = TemporalDateTime(DateTime(2024, 1, 2));
-  static final talk3StartTime = TemporalDateTime(DateTime(2024));
+  static final talk1StartTimeTemporal = TemporalDateTime(DateTime(2024, 1, 3));
+  static final talk2StartTimeTemporal = TemporalDateTime(DateTime(2024, 1, 2));
+  static final talk3StartTimeTemporal = TemporalDateTime(DateTime(2024));
+
+  static final talk1StartTime = talk1StartTimeTemporal.getDateTimeInUtc();
+  static final talk2StartTime = talk2StartTimeTemporal.getDateTimeInUtc();
+  static final talk3StartTime = talk3StartTimeTemporal.getDateTimeInUtc();
 
   static final talks = PaginatedResult(
     [
@@ -12,19 +16,19 @@ class TestHelpers {
         id: '1',
         title: 'Test Talk 1',
         room: 'Room 1',
-        startTime: talk1StartTime,
+        startTime: talk1StartTimeTemporal,
       ),
       Talk(
         id: '2',
         title: 'Test Talk 2',
         room: 'Room 2',
-        startTime: talk2StartTime,
+        startTime: talk2StartTimeTemporal,
       ),
       Talk(
         id: '3',
         title: 'Test Talk 3',
         room: 'Room 3',
-        startTime: talk3StartTime,
+        startTime: talk3StartTimeTemporal,
       ),
     ],
     null,
@@ -72,37 +76,37 @@ class TestHelpers {
   static final talkTimeSlots = PaginatedData(
     items: [
       TalkTimeSlot(
-        startTime: talk3StartTime.getDateTimeInUtc(),
+        startTime: talk3StartTime,
         talks: [
           TalkPreview(
             id: '3',
             title: 'Test Talk 3',
             room: 'Room 3',
-            startTime: talk3StartTime.getDateTimeInUtc(),
+            startTime: talk3StartTime,
             speakerNames: const ['Speaker 1', 'Speaker 2', 'Speaker 3'],
           ),
         ],
       ),
       TalkTimeSlot(
-        startTime: talk2StartTime.getDateTimeInUtc(),
+        startTime: talk2StartTime,
         talks: [
           TalkPreview(
             id: '2',
             title: 'Test Talk 2',
             room: 'Room 2',
-            startTime: talk2StartTime.getDateTimeInUtc(),
+            startTime: talk2StartTime,
             speakerNames: const ['Speaker 1', 'Speaker 2', 'Speaker 3'],
           ),
         ],
       ),
       TalkTimeSlot(
-        startTime: talk1StartTime.getDateTimeInUtc(),
+        startTime: talk1StartTime,
         talks: [
           TalkPreview(
             id: '1',
             title: 'Test Talk 1',
             room: 'Room 1',
-            startTime: talk1StartTime.getDateTimeInUtc(),
+            startTime: talk1StartTime,
             speakerNames: const ['Speaker 1', 'Speaker 2', 'Speaker 3'],
           ),
         ],
@@ -113,37 +117,37 @@ class TestHelpers {
   static final talkTimeSlotsJson = {
     'items': [
       {
-        'startTime': '2024-01-01T05:00:00.000Z',
+        'startTime': talk3StartTime.toIso8601String(),
         'talks': [
           {
             'id': '3',
             'title': 'Test Talk 3',
             'room': 'Room 3',
-            'startTime': '2024-01-01T05:00:00.000Z',
+            'startTime': talk3StartTime.toIso8601String(),
             'speakerNames': ['Speaker 1', 'Speaker 2', 'Speaker 3'],
           },
         ],
       },
       {
-        'startTime': '2024-01-02T05:00:00.000Z',
+        'startTime': talk2StartTime.toIso8601String(),
         'talks': [
           {
             'id': '2',
             'title': 'Test Talk 2',
             'room': 'Room 2',
-            'startTime': '2024-01-02T05:00:00.000Z',
+            'startTime': talk2StartTime.toIso8601String(),
             'speakerNames': ['Speaker 1', 'Speaker 2', 'Speaker 3'],
           },
         ],
       },
       {
-        'startTime': '2024-01-03T05:00:00.000Z',
+        'startTime': talk1StartTime.toIso8601String(),
         'talks': [
           {
             'id': '1',
             'title': 'Test Talk 1',
             'room': 'Room 1',
-            'startTime': '2024-01-03T05:00:00.000Z',
+            'startTime': talk1StartTime.toIso8601String(),
             'speakerNames': ['Speaker 1', 'Speaker 2', 'Speaker 3'],
           },
         ],
