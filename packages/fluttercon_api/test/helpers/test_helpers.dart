abd8afe79b7da3c3a20e3d21eff8a5f86a061904
Stefan <stefan@verygood.ventures>
2024-09-11 10:12:56 -0400

    api tests

--- packages/fluttercon_api/test/helpers/test_helpers.dart
+++ packages/fluttercon_api/test/helpers/test_helpers.dart
@@ -1,3 +1,5 @@
+import 'package:fluttercon_shared_models/fluttercon_shared_models.dart';
+
 class TestHelpers {
   static const userResponse = {'id': 'id', 'sessionToken': 'sessionToken'};
 
@@ -27,5 +29,28 @@ class TestHelpers {
         'imageUrl': 'imageUrl',
       },
     ],
+  };
+
+  static const userId = 'userId';
+  static const talkId = 'talkId';
+
+  static const createFavoriteRequest = CreateFavoriteRequest(
+    userId: userId,
+    talkId: talkId,
+  );
+
+  static final createFavoriteResponse = {
+    'userId': userId,
+    'talkId': talkId,
+  };
+
+  static const deleteFavoriteRequest = DeleteFavoriteRequest(
+    userId: userId,
+    talkId: talkId,
+  );
+
+  static final deleteFavoriteResponse = {
+    'userId': userId,
+    'talkId': talkId,
   };
 }
--- packages/fluttercon_api/test/src/fluttercon_api_test.dart
+++ packages/fluttercon_api/test/src/fluttercon_api_test.dart
@@ -42,13 +42,18 @@ void main() {
       required Uri url,
       int httpStatus = HttpStatus.ok,
       T? response,
+      HttpMethod method = HttpMethod.get,
       Exception? exception,
     }) {
       final whenCallback = when(
         () => client.send(
           any(
             that: isA<Request>()
-                .having((req) => req.method, 'method', 'GET')
+                .having(
+                  (req) => req.method,
+                  'method',
+                  method.name.toUpperCase(),
+                )
                 .having((req) => req.url, 'url', url),
           ),
         ),
@@ -316,6 +321,253 @@ void main() {
 
           expect(
             () async => flutterconApi.getTalks(),
+            throwsA(
+              isA<FlutterconApiClientException>(),
+            ),
+          );
+        },
+      );
+    });
+
+    group('addFavorite', () {
+      final uri = Uri.parse('$baseUrl/favorites');
+
+      test('returns $CreateFavoriteResponse on successful response', () async {
+        whenHttpClientSend(
+          url: uri,
+          response: TestHelpers.createFavoriteResponse,
+          method: HttpMethod.post,
+        );
+
+        final response = await flutterconApi.addFavorite(
+          request: TestHelpers.createFavoriteRequest,
+        );
+
+        expect(response, isA<CreateFavoriteResponse>());
+      });
+
+      test(
+        'throws $FlutterconApiMalformedResponseException '
+        'when body is malformed',
+        () async {
+          whenHttpClientSend(
+            url: uri,
+            response: '',
+            method: HttpMethod.post,
+          );
+
+          expect(
+            () async => flutterconApi.addFavorite(
+              request: TestHelpers.createFavoriteRequest,
+            ),
+            throwsA(isA<FlutterconApiMalformedResponseException>()),
+          );
+        },
+      );
+
+      test(
+        'throws $FlutterconApiClientException '
+        'when response is not successful',
+        () async {
+          whenHttpClientSend(
+            url: uri,
+            // ignore: inference_failure_on_collection_literal
+            response: {},
+            method: HttpMethod.post,
+            httpStatus: HttpStatus.notFound,
+          );
+
+          expect(
+            () async => flutterconApi.addFavorite(
+              request: TestHelpers.createFavoriteRequest,
+            ),
+            throwsA(
+              isA<FlutterconApiClientException>().having(
+                (e) => e.statusCode,
+                'status code',
+                equals(
+                  HttpStatus.notFound,
+                ),
+              ),
+            ),
+          );
+        },
+      );
+
+      test(
+        'throws $FlutterconApiClientException '
+        'when an unexpected error occurs',
+        () async {
+          whenHttpClientSend<CreateFavoriteResponse>(
+            url: uri,
+            method: HttpMethod.post,
+            exception: Exception('oops'),
+          );
+
+          expect(
+            () async => flutterconApi.addFavorite(
+              request: TestHelpers.createFavoriteRequest,
+            ),
+            throwsA(
+              isA<FlutterconApiClientException>(),
+            ),
+          );
+        },
+      );
+    });
+
+    group('removeFavorite', () {
+      final uri = Uri.parse('$baseUrl/favorites');
+
+      test('returns $DeleteFavoriteResponse on successful response', () async {
+        whenHttpClientSend(
+          url: uri,
+          response: TestHelpers.deleteFavoriteResponse,
+          method: HttpMethod.delete,
+        );
+
+        final response = await flutterconApi.removeFavorite(
+          request: TestHelpers.deleteFavoriteRequest,
+        );
+
+        expect(response, isA<DeleteFavoriteResponse>());
+      });
+
+      test(
+        'throws $FlutterconApiMalformedResponseException '
+        'when body is malformed',
+        () async {
+          whenHttpClientSend(
+            url: uri,
+            response: '',
+            method: HttpMethod.delete,
+          );
+
+          expect(
+            () async => flutterconApi.removeFavorite(
+              request: TestHelpers.deleteFavoriteRequest,
+            ),
+            throwsA(isA<FlutterconApiMalformedResponseException>()),
+          );
+        },
+      );
+
+      test(
+        'throws $FlutterconApiClientException '
+        'when response is not successful',
+        () async {
+          whenHttpClientSend(
+            url: uri,
+            // ignore: inference_failure_on_collection_literal
+            response: {},
+            method: HttpMethod.delete,
+            httpStatus: HttpStatus.notFound,
+          );
+
+          expect(
+            () async => flutterconApi.removeFavorite(
+              request: TestHelpers.deleteFavoriteRequest,
+            ),
+            throwsA(
+              isA<FlutterconApiClientException>().having(
+                (e) => e.statusCode,
+                'status code',
+                equals(
+                  HttpStatus.notFound,
+                ),
+              ),
+            ),
+          );
+        },
+      );
+
+      test(
+        'throws $FlutterconApiClientException '
+        'when an unexpected error occurs',
+        () async {
+          whenHttpClientSend<DeleteFavoriteResponse>(
+            url: uri,
+            method: HttpMethod.delete,
+            exception: Exception('oops'),
+          );
+
+          expect(
+            () async => flutterconApi.removeFavorite(
+              request: TestHelpers.deleteFavoriteRequest,
+            ),
+            throwsA(
+              isA<FlutterconApiClientException>(),
+            ),
+          );
+        },
+      );
+    });
+
+    group('getFavorites', () {
+      const userId = 'userId';
+      final url = Uri.parse('$baseUrl/favorites/$userId');
+
+      test(
+        'returns ${PaginatedData<TalkTimeSlot>} on successful response',
+        () async {
+          whenHttpClientSend(url: url, response: TestHelpers.talksResponse);
+
+          final talks = await flutterconApi.getFavorites(userId: userId);
+
+          expect(talks, isA<PaginatedData<TalkTimeSlot>>());
+        },
+      );
+
+      test(
+        'throws $FlutterconApiMalformedResponseException '
+        'when body is malformed',
+        () async {
+          whenHttpClientSend(url: url, response: '');
+
+          expect(
+            () async => flutterconApi.getFavorites(userId: userId),
+            throwsA(isA<FlutterconApiMalformedResponseException>()),
+          );
+        },
+      );
+
+      test(
+        'throws $FlutterconApiClientException '
+        'when response is not successful',
+        () async {
+          whenHttpClientSend(
+            url: url,
+            // ignore: inference_failure_on_collection_literal
+            response: {},
+            httpStatus: HttpStatus.notFound,
+          );
+
+          expect(
+            () async => flutterconApi.getFavorites(userId: userId),
+            throwsA(
+              isA<FlutterconApiClientException>().having(
+                (e) => e.statusCode,
+                'status code',
+                equals(
+                  HttpStatus.notFound,
+                ),
+              ),
+            ),
+          );
+        },
+      );
+
+      test(
+        'throws $FlutterconApiClientException '
+        'when an unexpected error occurs',
+        () async {
+          whenHttpClientSend<TalkTimeSlot>(
+            url: url,
+            exception: Exception('oops'),
+          );
+
+          expect(
+            () async => flutterconApi.getFavorites(userId: userId),
             throwsA(
               isA<FlutterconApiClientException>(),
             ),
