f6ed34934798e9a84f61a9aa25039f450b37ed0f
Stefan <stefan@verygood.ventures>
2024-09-16 11:49:48 -0400

    test fix

--- api/packages/fluttercon_shared_models/test/src/helpers/test_helpers.dart
+++ api/packages/fluttercon_shared_models/test/src/helpers/test_helpers.dart
@@ -93,6 +93,7 @@ class TestHelpers {
     'room': 'room',
     'speakerNames': <String>[],
     'startTime': '2024-01-01T00:00:00.000',
+    'isFavorite': false,
   };
 
   static final talkTimeSlot = TalkTimeSlot(
